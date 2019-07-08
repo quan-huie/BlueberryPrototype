1c016a392b211588795c823c1c9774bc26492edc
Kai Bustos <bustoskai@gmail.com>
2019-07-08 15:26:06 -0700

    Initial Commit

---
+++ BlueberryPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueberryPrototype
+//
+//  Created by Kai Alexander Bustos on 7/8/19.
+//  Copyright © 2019 Kai Alexander Bustos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
